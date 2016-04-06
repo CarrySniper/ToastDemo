53c77971b7f5aca98e23e6a534349707185acfd4
CK-chan <692771080@qq.com>
2016-04-06 17:48:58 +0800

    修改提示文字

--- ToastDemo/ViewController.m
+++ ToastDemo/ViewController.m
@@ -19,20 +19,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+    self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
     
     //要注意，直接放这里是不显示的。
     //[[UIApplication sharedApplication]keyWindow]还没有加载
     
     UILabel *download = [[UILabel alloc]initWithFrame:CGRectMake(10, 180, self.view.frame.size.width-20, 80)];
-    download.text = @"demo下载地址：\nhttps://github.com/cjq002/ToastDemo.git";
+    download.text = @"demo下载地址：\n https://github.com/cjq002/ToastDemo.git";
     download.textAlignment = NSTextAlignmentCenter;
     download.numberOfLines = 0;
     [self.view addSubview:download];
 }
 
 - (IBAction)showAction:(id)sender {
-    [[Toast shareInstance] makeToast:@"Toast提示框" duration:3.0];
+    [[Toast shareInstance] makeToast:@"Toast提示框，3秒后自动消失。" duration:3.0];
 }
 
 - (void)didReceiveMemoryWarning {
